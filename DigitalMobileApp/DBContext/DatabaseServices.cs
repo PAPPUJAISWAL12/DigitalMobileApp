443b8d800e294610fcd024071af9fa005d7c4f21
PAPPUJAISWAL12 <Jaiswalpappu186@gmail.com>
2024-09-28 19:32:53 +0545

    Add project files.

---
+++ DigitalMobileApp/DBContext/DatabaseServices.cs
@@ -0,0 +1,44 @@
+﻿using DigitalMobileApp.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalMobileApp.DBContext
+{
+    
+    class DatabaseServices
+    {
+        private readonly SQLiteAsyncConnection _context;
+
+        public DatabaseServices(string dbpath)
+        {
+            _context =new SQLiteAsyncConnection(dbpath);
+            InitializeDatabase();
+        }
+        private async void InitializeDatabase()
+        {
+            await _context.CreateTableAsync<Notes>();
+        }
+        public Task<List<Notes>> GetNoteList()
+        {
+            return _context.Table<Notes>().ToListAsync();
+        }
+
+        public async Task AddNotes(Notes note)
+        {
+            await _context.InsertAsync(note);
+        }
+
+        public async void DeleteNotes(Notes n)
+        {
+            await _context.DeleteAsync(n);
+        }
+        public async Task UpdateNotes(Notes note)
+        {
+            await _context.UpdateAsync(note);
+        }
+    }
+}
---
+++ DigitalMobileApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DigitalMobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
